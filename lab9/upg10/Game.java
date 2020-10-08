29bb2afc3eb7e4159f208edc9371731b7f709d1e
SpaceCrab <lundin.eric@hotmail.com>
2020-10-08 20:02:37 +0200

    försök på game.jav men inte komplett, behöver mer brain juice

--- lab9/upg10/Game.java
+++ lab9/upg10/Game.java
@@ -1,22 +1,25 @@
 public class Game
 {
   public static void main(String[] args) {
-    int[][] map = new int[4][5];
+    int[][] map = {{1,1,2,3,3},{2,1,1,2,3},{3,2,2,1,2},{3,3,3,3,3}};
     boolean[][] deathMap = new boolean[4][5];
     int lengthVert = map.length;
     int lengthHor = map[0].length;
+    int neighbors = 0;
 
-    for (int i = 0;i < lengthVert ;i++ ) {
-      for (int a = 0; a < lengthHor ;i++ ) {
+    for (int i = 0;i < lengthVert;i++ ) {
+      for (int a = 0; a < lengthHor ;a++ ) {
 
-        int neighbors = map[i+1][a] + map[i-1][a] + map[i][a+1] + map[i][a-1]+
-         map[i+1][a-1] + map[i+1][a+1] + map[i-1][a+1] + map[i-1][a-1];
+        //if(i > 0 && a >0 && i < 3 && a < 4) {
+          neighbors = map[i+1][a] + map[i-1][a] + map[i][a+1] + map[i][a-1]+
+           map[i+1][a-1] + map[i+1][a+1] + map[i-1][a+1] + map[i-1][a-1];
+        //}
 
         if (map[i][a] >= 3 || neighbors >= 15) {
           deathMap[i][a] = false;
         }
-        else {
-          deathMap[i][a] = false;
+        else{
+          deathMap[i][a] = true;
         }
       }
     }
