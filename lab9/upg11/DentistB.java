9e90bc1ae3cf4b259f2c3bbc3c5220724365e968
SpaceCrab <lundin.eric@hotmail.com>
2020-10-08 18:50:52 +0200

    klar med upg11 a och b

--- lab9/upg11/DentistB.java
+++ lab9/upg11/DentistB.java
@@ -1,9 +1,9 @@
 import java.util.Scanner;
 import java.lang.*;
 
-public class DentistA{
+public class DentistB{
 
-  static void printArr(String[][] arr){
+  static void printArr2d(String[][] arr){
 
     for (int i = 0; i < arr.length; i++ ) {
       for (int a = 0; a < arr[0].length;a++ ) {
@@ -13,28 +13,41 @@ public class DentistA{
     }
   }
 
+  static void printArr1d(String[] arr){
+    for (int i = 0;i < arr.length ;i++ ) {
+      if (arr[i] != null) {
+        System.out.println(arr[i]);
+      }
+    }
+  }
+
   public static void main(String[] args) {
     Scanner input = new Scanner(System.in);
 
     String[][] treatment = {{"1: Kontroll" ,"600"},{"2: Lagning hål","1500"},{"3: Rengöring","300"}};
+    String[] receipt = new String[10];
     boolean done = false;
     double cost = 0.0;
+    int count = 0;
     System.out.println("välj en behandling\nskriv -1 när du är klar\n\n\n");
 
 
     do {
-      printArr(treatment);
+      printArr2d(treatment);
       int choice = input.nextInt();
 
       switch (choice) {
         case 1:
           cost = cost + Double.parseDouble(treatment[0][1]);
+          receipt[count] = treatment[0][0] + "        " + treatment[0][1];
           break;
         case 2:
           cost = cost + Double.parseDouble(treatment[1][1]);
+          receipt[count] = treatment[1][0] + "    " + treatment[1][1];
           break;
         case 3:
           cost = cost + Double.parseDouble(treatment[2][1]);
+          receipt[count] = treatment[2][0] + "      " + treatment[2][1];
           break;
         case 4:
           done = true;
@@ -44,7 +57,10 @@ public class DentistA{
           done = true;
           break;
       }
+      count++;
     } while (!done);
+
+    printArr1d(receipt);
 
     System.out.println("det slutgiltiga priset blev " + cost);
   }
