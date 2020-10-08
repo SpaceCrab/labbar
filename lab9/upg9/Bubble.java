73e9e973e2893d17f04932ac34d138b0e775d87a
eric <lundin.eric@hotmail.com>
2020-10-08 14:40:04 +0200

    ljlkjtest iegn

---
+++ lab9/upg9/Bubble.java
@@ -0,0 +1,27 @@
+public class Bubble{
+
+  public static void main(String[] args) {
+    int[] array = {32, 27, 64, 18, 95, 14, 90, 70, 60, 37};
+    int length = array.length;
+
+    System.out.println("osorterad array");
+    for (int i = 0; i < length; i++) {
+      System.out.print(array[i] + " ");
+    }
+
+    for (int i = 0; i < length; i++) {
+      for (int a = i;a < length - i -1 ;a++ ) {
+        if (array[a] > array[a+1]) {
+          int temp = array[a];
+          array[a] = array[a+1];
+          array[a+1] = temp;
+        }
+      }
+    }
+
+    System.out.println("\nsorterad array");
+    for (int i = 0; i < length; i++) {
+      System.out.print(array[i] + " ");
+    }
+  }
+}
