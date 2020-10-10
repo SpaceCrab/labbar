73142b0511efeb3e2d455e49dfb0b9323918597e
SpaceCrab <lundin.eric@hotmail.com>
2020-10-10 18:04:16 +0200

    Update DentistB.java

--- lab9/upg11/DentistB.java
+++ lab9/upg11/DentistB.java
@@ -3,25 +3,32 @@ import java.lang.*;
 
 public class DentistB{
 
-  static void printArr2d(String[][] arr){
+  static void printArr2d(String[][] arr)
+  {
 
-    for (int i = 0; i < arr.length; i++ ) {
-      for (int a = 0; a < arr[0].length;a++ ) {
+    for (int i = 0; i < arr.length; i++ )
+    {
+      for (int a = 0; a < arr[0].length;a++ )
+      {
         System.out.print(arr[i][a] + " ");
       }
       System.out.println("");
     }
   }
 
-  static void printArr1d(String[] arr){
-    for (int i = 0;i < arr.length ;i++ ) {
-      if (arr[i] != null) {
+  static void printArr1d(String[] arr)
+  {
+    for (int i = 0;i < arr.length ;i++ )
+    {
+      if (arr[i] != null)
+      {
         System.out.println(arr[i]);
       }
     }
   }
 
-  public static void main(String[] args) {
+  public static void main(String[] args)
+  {
     Scanner input = new Scanner(System.in);
 
     String[][] treatment = {{"1: Kontroll" ,"600"},{"2: Lagning hål","1500"},{"3: Rengöring","300"}};
@@ -32,11 +39,13 @@ public class DentistB{
     System.out.println("välj en behandling\nskriv -1 när du är klar\n\n\n");
 
 
-    do {
+    do
+    {
       printArr2d(treatment);
       int choice = input.nextInt();
 
-      switch (choice) {
+      switch (choice)
+      {
         case 1:
           cost = cost + Double.parseDouble(treatment[0][1]);
           receipt[count] = treatment[0][0] + "        " + treatment[0][1];
